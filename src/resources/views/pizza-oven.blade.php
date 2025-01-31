b1cfcb0c0d131577060f58327764b82025a10d74
waffle.lord <waffle.lord@hotmail.com>
2025-01-31 12:53:14 -0500

    main view WIP

---
+++ src/resources/views/pizza-oven.blade.php
@@ -0,0 +1,43 @@
+
+@auth
+    <x-app-layout>
+        <x-slot name="header">
+
+        </x-slot>
+        <h1>Hello There</h1>
+    </x-app-layout>
+@else
+    <x-guest-layout>
+        <div class="px-6 py-2 dark:bg-gray-900 dark:text-gray-100">
+            @if (Route::has('login'))
+                <nav class="-mx-3 flex flex-1 justify-end">
+                    @auth
+                        <a
+                            href="{{ url('/dashboard') }}"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Dashboard
+                        </a>
+                    @else
+                        <a
+                            href="{{ route('login') }}"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Log in
+                        </a>
+
+                        @if (Route::has('register'))
+                            <a
+                                href="{{ route('register') }}"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                Register
+                            </a>
+                        @endif
+                    @endauth
+                </nav>
+            @endif
+        </div>
+        <h1 class="bg-red-600 dark:text-gray-100">Hello There</h1>
+    </x-guest-layout>
+@endauth
