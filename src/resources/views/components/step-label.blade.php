342e5a814eaa7c173380e85db27ecdfc91286426
waffle.lord <waffle.lord@hotmail.com>
2025-02-22 10:21:17 -0500

    update frontend

--- src/resources/views/components/step-label.blade.php
+++ src/resources/views/components/step-label.blade.php
@@ -1,13 +1,33 @@
-@props(['label'])
+@props(['label', 'position', 'current'])
 
-<div class="grid justify-items-center relative invisible lg:visible md:visible">
-    <div class="rounded-full bg-black h-12 w-12">
-    </div>
-    <div class="absolute -bottom-10 text-center">{{ $label }}</div>
+<div class="grid justify-items-center relative hidden lg:flex md:flex">
+    @if($position < $current)
+        <div class="rounded-full bg-green-500 h-12 w-12">
+        </div>
+        <div class="absolute top-12 text-center w-full flex-wrap">{{ $label }}</div>
+    @elseif($position === $current)
+        <div class="rounded-full bg-indigo-500 h-12 w-12">
+        </div>
+        <div class="absolute top-12 text-center w-full flex-wrap">{{ $label }}</div>
+    @else
+        <div class="rounded-full bg-black h-12 w-12">
+        </div>
+        <div class="absolute top-12 text-center w-full flex-wrap">{{ $label }}</div>
+    @endif
 </div>
 
-<div class="grid justify-items-center justify-start relative md:hidden lg:hidden">
-    <div class="rounded-full bg-black h-12 w-12">
-    </div>
-    <div class="absolute -bottom-10 text-center">{{ $label }}</div>
+<div class="relative md:hidden lg:hidden">
+    @if($position < $current)
+        <div class="rounded-full bg-green-500 h-12 w-12">
+        </div>
+        <div class="absolute top-3 left-14">{{ $label }}</div>
+    @elseif($position === $current)
+        <div class="rounded-full bg-indigo-500 h-12 w-12">
+        </div>
+        <div class="absolute top-3 left-14">{{ $label }}</div>
+    @else
+        <div class="rounded-full bg-black h-12 w-12">
+        </div>
+        <div class="absolute top-3 left-14">{{ $label }}</div>
+    @endif
 </div>
--- src/resources/views/livewire/pizza-order-info.blade.php
+++ src/resources/views/livewire/pizza-order-info.blade.php
@@ -1,31 +1,43 @@
 <div class="w-full text-center bg-[#e1e1e1] dark:bg-[#212f4d]">
-    <h1 class="text-3xl p-6 text-blue-500 dark:text-indigo-500">Order #{{ $order->order_number }}</h1>
-    <div class="grid grid-cols-9 justify-items-center p-20 invisible md:visible lg:visible">
-        @for($i = 0; $i < count($steps); $i++)
-            <x-step-label :label="$steps[$i]" />
-            @if($i < count($steps) - 1)
+    <h1 class="text-3xl text-blue-500 dark:text-indigo-500">Order #{{ $order->order_number }}</h1>
+    <p class="text-sm/relaxed text-gray-700 dark:text-gray-400">This page refreshes automatically :)</p>
+    <div class="grid grid-cols-9 justify-items-center p-20 hidden md:flex lg:flex">
+        @for($i = 0; $i < count($steps) - 1; $i++)
+            <x-step-label :label="$steps[$i]" :position="$i" :current="$order->current_step"/>
+            @if($i < count($steps))
                 <div class="flex items-center w-full">
-                    <div class="w-full bg-gray-400 rounded-full h-2.5 dark:bg-[#111827]">
-                        <div class="bg-blue-500 dark:bg-indigo-500 h-2.5 rounded-full" style="width: {{ $order->step_progress }}%"></div>
+                    <div class="w-full bg-gray-400 h-2.5 dark:bg-[#111827]">
+                        @if($i < $order->current_step)
+                            <div class="bg-blue-500 dark:bg-indigo-500 h-2.5" style="width: 100%"></div>
+                        @elseif($i == $order->current_step)
+                            <div class="bg-blue-500 dark:bg-indigo-500 h-2.5 rounded-r-md" style="width: {{ $order->step_progress }}%"></div>
+                        @else
+                            <div class="bg-blue-500 dark:bg-indigo-500 h-2.5 rounded-r-md" style="width: 0"></div>
+                        @endif
                     </div>
                 </div>
             @endif
         @endfor
+        <x-step-label label="Done" :position="$i" :current="$order->current_step"/>
+        <div class="bg-blue-500 dark:bg-indigo-500 h-2.5 rounded-r-md" style="width: 0"></div>
 
     </div>
-    <div class="lg:hidden md:hidden">
-        @for($i = 0; $i < count($steps); $i++)
-            <x-step-label :label="$steps[$i]" />
-            @if($i < count($steps) - 1)
-                <div class="flex items-center w-full">
-                    <div class="w-full bg-gray-400 rounded-full h-2.5 dark:bg-[#111827]">
-                        <div class="bg-blue-500 dark:bg-indigo-500 h-2.5 rounded-full" style="width: {{ $order->step_progress }}%"></div>
-                    </div>
+    <div class="py-6 lg:hidden md:hidden">
+        @for($i = 0; $i < count($steps) - 1; $i++)
+            <div class="py-2 flex flex-row relative">
+                <x-step-label :label="$steps[$i]" :position="$i" :current="$order->current_step"/>
+            @if($i < count($steps))
+                <div class="flex flex-row w-full text-end">
+                    @if($i < $order->current_step)
+                        <p class="absolute w-full text-center top-5">Done</p>
+                    @elseif($i == $order->current_step)
+                        <p class="absolute w-full text-center top-5 text-blue-500 dark:text-indigo-500 font-medium">{{ $order->step_progress }}%</p>
+                    @endif
                 </div>
             @endif
+            </div>
         @endfor
 
     </div>
     <p>{{ $order->message }}</p>
-    <p class="text-sm/relaxed text-gray-700 dark:text-gray-400">This page refreshes automatically :)</p>
 </div>
