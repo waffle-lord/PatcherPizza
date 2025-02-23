8fb088ceb617acbd6e540d903b818e86a9deb71e
waffle.lord <waffle.lord@hotmail.com>
2025-02-22 19:57:23 -0500

    add last order to closed kitchen info

--- src/resources/views/components/last-order.blade.php
+++ src/resources/views/components/last-order.blade.php
@@ -2,7 +2,13 @@
 
 
 @if($lastOrder !== null)
-    <div>
-        <p>{{$lastOrder->order_number}}</p>
+    <div class="flex lg:w-1/3 md:w-1/2 w-full h-10 items-center justify-center rounded-lg bg-gray-50 dark:dark:bg-[#111827]">
+        <div class="text-green-600 dark:text-green-700">
+            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M0 0h24v24H0z" fill="none"></path><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path>
+            </svg>
+        </div>
+        <span class="ml-2 mr-2">Last Completed Order:</span>
+        <span class="text-green-600 dark:text-green-700 font-bold text-lg">#{{$lastOrder->order_number}}</span>
     </div>
 @endif
--- src/resources/views/livewire/pizza-oven-info.blade.php
+++ src/resources/views/livewire/pizza-oven-info.blade.php
@@ -2,9 +2,10 @@
     @if($order === null)
         <div class="w-full text-center">
             <p>The kitchen is closed</p>
-            <p class="text-sm/relaxed text-gray-700 dark:text-gray-400">This page refreshes automatically
-                :)</p>
-            <x-last-order :lastOrder="$lastOrder"/>
+            <p class="text-sm/relaxed text-gray-700 dark:text-gray-400">This page refreshes automatically :)</p>
+            <div class="flex w-full justify-center items-center p-6">
+                <x-last-order :lastOrder="$lastOrder" />
+            </div>
         </div>
     @else
         <livewire:pizza-order-info :order="$order"/>
